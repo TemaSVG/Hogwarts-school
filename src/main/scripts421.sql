e034b30a3f5240f072f1652d6e96b4c7b41d960d
Артём <temasvg@gmail.com>
2025-06-29 23:20:06 +1000

    Home Work 4.1

--- src/main/scripts421.sql
+++ src/main/scripts421.sql
@@ -1,12 +1,13 @@
 ALTER TABLE student
-    MODIFY age INT NOT NULL DEFAULT 20,
+    ALTER COLUMN age SET NOT NULL,
+    ALTER COLUMN age SET DEFAULT 20,
     ADD CONSTRAINT chk_student_age CHECK (age >= 16);
 
 ALTER TABLE student
-    MODIFY name VARCHAR(255) NOT NULL,
+    ALTER COLUMN name SET NOT NULL,
     ADD CONSTRAINT uq_student_name UNIQUE (name);
 
 ALTER TABLE faculty
-    MODIFY name VARCHAR(255) NOT NULL,
-    MODIFY color VARCHAR(255) NOT NULL,
+    ALTER COLUMN name SET NOT NULL,
+    ALTER COLUMN color SET NOT NULL,
     ADD CONSTRAINT uq_faculty_name_color UNIQUE (name, color);
